d5db125a251ef84a643b2162d702007ccb2e15f8
Corina Talavera <corina.talavera@gmail.com>
2018-11-18 23:01:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,17 @@
 class Song
-  attr_accessor :artist, :name
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
   end
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
-
   def self.new_by_filename(file)
-    song_info = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
-    song.artist_name = song_info[0]
-    song
+    artist = file.split(" - ")[0]
+    song_name = file.split(" - ")[1]
+    foo = self.new(song_name)
+    foo.artist = Artist.find_or_create_by_name(artist)
+    foo.artist.save
+    foo.artist.add_song(song_name)
+    return foo
   end
 end
